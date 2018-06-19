b592079ae3b7713f700d5472982432b3cf630d85
Alex <Alex@DESKTOP-ALEX>
2018-06-19 17:39:25 +0200

    Creation validateur

---
+++ ClassLibrary1/Class/Validateur/Object.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Outils.Class.Validateur
+{
+    /// <summary>
+    /// Valide des objets.
+    /// </summary>
+    public static class Object
+    {
+        /// <summary>
+        /// Retourne une exception si le paramètre est null.
+        /// </summary>
+        /// <typeparam name="T">N'importe quel type.</typeparam>
+        /// <param name="t">Paramètre.</param>
+        /// <param name="nomParametre">Nom du paramètre.</param>
+        public static void NonNull<T>(this T t,  string nomParametre)
+        {
+            if(t == null)
+            {
+                throw new ArgumentNullException($"Le paramètre {nomParametre} est null");
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/Class/Validateur/String.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Outils.Class.Validateur
+{
+    /// <summary>
+    /// Valide des objets de type string.
+    /// </summary>
+    public static class String
+    {
+        /// <summary>
+        /// Vérification que le string est non null.
+        /// </summary>
+        /// <param name="nomParametre">Nom du paramètre.</param>
+        public static void NonNull(this string s,  string nomParametre)
+        {
+            if(s == null)
+            {
+                throw new ArgumentNullException($"Le paramètre {nomParametre} est null");
+            }
+        }
+
+        /// <summary>
+        /// Renvoi une exeption si le paramètre n'est pas renseigné.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="nomParametre"></param>
+        public static void Obligatoire(this string s, string nomParametre)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                throw new ArgumentException($"Le paramètre {nomParametre} doit-être renseigné.");
+            }
+        }
+    }
+}
